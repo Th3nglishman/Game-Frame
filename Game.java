c3823d2a27ee799eaf1cf1e039c1447e5af9018e
MasterEL <50185565+MasterEL@users.noreply.github.com>
2019-05-17 14:16:41 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,5 +1,7 @@
 import javax.swing.JPanel;
 
+// Description: is a super class for all game files
+
 public abstract class Game extends JPanel{
 	private static final long serialVersionUID = 1L;
 	public abstract String getName();
