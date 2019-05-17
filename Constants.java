4f7491fb82fc98a4680b5c15c40f2126d6312f14
MasterEL <50185565+MasterEL@users.noreply.github.com>
2019-05-17 14:09:17 -0700

    Update Constants.java

--- Constants.java
+++ Constants.java
@@ -1,4 +1,4 @@
-
+// Description: Stores public static variables for testing and ease of access across all instances ie: testing mode?
 public class Constants {
 	public static final boolean MAX=false;
 	public static final boolean TEST=false;
