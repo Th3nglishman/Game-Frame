0ebfb33fa1217d4459a1aa132c3cb169ac43605e
Th3nglishman <50185477+Th3nglishman@users.noreply.github.com>
2019-05-17 14:45:05 -0700

    Add files via upload

--- TextBased.java
+++ TextBased.java
@@ -15,15 +15,24 @@ public class TextBased extends Game {
 		System.out.println("\f");
 		System.out.println("Welcome to my GameFrame, why don't you stay awhile?");
 		userInput=kboard.next();
-		System.out.println("As you can see, here you proceed when you press a button");
-		userInput=kboard.next();
-		System.out.println("In the future you will be given the ability to change settings and other such");
-		userInput=kboard.next();
-		System.out.println("Currently this is a dead end in good style as it shows the possibility of more games while not creating more games");
-		userInput=kboard.next();
-		System.out.println("Clever no?");
-		userInput=kboard.next();
-		System.out.println("This message will self destruct");
-		
+		if (userInput.equalsIgnoreCase("no")) {
+			System.out.println("This message will self destruct");
+		}
+		else {
+			System.out.println("As you can see, here you proceed when you press a button");
+			userInput=kboard.next();
+			System.out.println("In the future you will be given the ability to change settings and other such");
+			userInput=kboard.next();
+			System.out.println("Currently this is a dead end in good style as it shows the possibility of more games while not creating more games");
+			userInput=kboard.next();
+			System.out.println("Clever no?");
+			userInput=kboard.next();
+			if (userInput.equalsIgnoreCase("no")) {
+				System.out.println("This message will self destruct");
+			}
+			else if (userInput.equalsIgnoreCase("Yes")||userInput.equalsIgnoreCase("yes!")) {
+				System.out.println("Thank you!");
+			}
+		}
 	}
 }
