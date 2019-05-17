0b6529fd6bc17dfcf41bd1ad972d60235357a636
Th3nglishman <50185477+Th3nglishman@users.noreply.github.com>
2019-05-17 15:03:26 -0700

    Update GraphicsGame.java

--- GraphicsGame.java
+++ GraphicsGame.java
@@ -2,6 +2,8 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyListener;
 
+// Description: Provides a basic class for all graphics based games to use
+
 public abstract class GraphicsGame extends Game implements KeyListener {
 	private static final long serialVersionUID = 1L;
 
