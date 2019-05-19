410a50308f0967cb3f0366c1236cd02d2a3206e2
Th3nglishman <50185477+Th3nglishman@users.noreply.github.com>
2019-05-19 15:22:22 -0700

    Delete Constants.java

--- Constants.java
+++
@@ -1,5 +0,0 @@
-// Description: Stores public static variables for testing and ease of access across all instances ie: testing mode?
-public class Constants {
-	public static final boolean MAX=false;
-	public static final boolean TEST=false;
-}
